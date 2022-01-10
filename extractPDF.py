842b15f31bec213b1f9d5fe0c6d378d1aa9357e1
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2022-01-09 19:19:51 -0600

    Added Feature to extract .msg attatchments

--- extractPDF.py
+++ extractPDF.py
@@ -3,6 +3,8 @@ from PyPDF2 import PdfFileReader
 import glob, os, shutil, xlsxwriter
 import pandas as pd
 import numpy as np
+import extract_msg
+import win32com.client
 
 path = os.getcwd()
 
@@ -21,7 +23,47 @@ append = path + "\Append Complete"
 if not os.path.exists(append):
     os.makedirs(append)
 
+emails = path + "\Old Emails"
+if not os.path.exists(emails):
+    os.makedirs(emails)
+
 #print(path)
+
+#for file in glob.glob(path + "\*.msg"):
+    #msg = extract_msg.openMsg(file)
+    #outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    #msg = outlook.OpenSharedItem(file)        
+    #att = msg.attachments
+    #for i in att:
+        #i.SaveAsFile(os.path.join(path, i.FileName))
+
+    #filename = file[len(path)+1:]
+
+    #print(filename)
+    #print('path: ',  path + "\Old Emails\Completed - " + filename)
+    #shutil.move(file, path + "\Old Emails\Completed - " + filename)
+
+for file in glob.glob(path + "\*.msg"):
+    filename = file[len(path)+1:]
+    movedFile = path + "\Old Emails\Completed - " + filename
+    shutil.move(file, movedFile)
+
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+    msg = outlook.OpenSharedItem(movedFile)        
+    att = msg.attachments
+    for i in att:
+        name = os.path.join(path, i.FileName)
+        dup = 0
+
+        def checkName(name, dup):
+            for file in glob.glob(path + "\*.pdf"):
+                if file == name:
+                    dup = dup + 1
+                    name = os.path.join(path, str(dup) + " - " + i.FileName)
+                    return checkName(name, dup)
+            return name
+
+        i.SaveAsFile(checkName(name, dup))
 
 for file in glob.glob(path + "\*.pdf"):
     
@@ -60,8 +102,8 @@ for file in glob.glob(path + "\*.pdf"):
 
     filename = file[len(path)+1:]
 
-    print(filename)
-    print('path: ',  path + "\Extract Complete\Completed - " + filename)
+    #print(filename)
+    #print('path: ',  path + "\Extract Complete\Completed - " + filename)
     shutil.move(file, path + "\Extract Complete\Completed - " + filename)
 
 for file in glob.glob(path + "\*.xlsx"):
